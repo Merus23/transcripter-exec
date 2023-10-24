7c91114207411def48a8382dbdd08f76842041ba
Merus23 <mateus.satira@gmail.com>
2023-10-24 13:01:04 -0300

    feat: finish the transcripter structure

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import whisper
+import sys
 
-def transcripter(source):
+def transcripter(source, output):
     source = str(source)
-    model = whisper.load_model('medium')
+    model = whisper.load_model('smal')
     result = model.transcribe(source) 
     
 
-    
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: python main.py <source path>")
+        sys.exit(1)
+
+    source = sys.argv[1]
+    output = sys.argv[2]
+
+    transcripter(source=source, output=output)
