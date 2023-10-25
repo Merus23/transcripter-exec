8fefa0751a7fa1b8122ab21752d8b1e81d42ef9b
Merus23 <mateus.satira@gmail.com>
2023-10-25 13:04:09 -0300

    feat: finish linux project

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import whisper
 import sys
+import platform
 
-def transcripter(source, output):
+def transcripter(source):
     source = str(source)
-    model = whisper.load_model('smal')
+    model = whisper.load_model('small')
     result = model.transcribe(source) 
+    text = result['text']
     
 
+    so = platform.system()
+    if so == 'Windows':
+        name = source.split('\\')[-1].split('.')[0]
+    else:
+        name = source.split('/')[-1].split('.')[0]
+   
+    
+    with open(name + '.txt', 'a') as file:
+        file.write(text)    
+
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 2:
         print("Usage: python main.py <source path>")
         sys.exit(1)
-
+    
     source = sys.argv[1]
-    output = sys.argv[2]
 
-    transcripter(source=source, output=output)
+
+    transcripter(source=source)
